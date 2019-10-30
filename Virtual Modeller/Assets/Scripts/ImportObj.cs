bffc088ebeccc7ec2c91ed9b1d87190affd60517
Dennis ZL Liu <zlliu514@gmail.com>
2019-10-30 17:06:44 -0400

    working on issue #10

--- Virtual Modeller/Assets/Scripts/ImportObj.cs
+++ Virtual Modeller/Assets/Scripts/ImportObj.cs
@@ -9,16 +9,21 @@ public class ImportObj : MonoBehaviour {
 
 	private string fileDir = "";
 
-	public void showDialog(){
+	public void init(){
 
-        Debug.Log("0000000000000");	
+		Camera cam = Camera.main;
+
+		Debug.Log("0000000000000");	
 		ExtensionFilter[] extensionList = new [] {
                 new ExtensionFilter("Waveform obj", "obj")
             };
 		string[] path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensionList, false);
 
 		fileResult(path);
-		loadModel(fileDir);
+		GameObject gameObject = loadModel(fileDir);
+
+		gameObject.transform.position = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10f));
+
 	}
 
 	public void fileResult(string[] p) {
@@ -27,7 +32,7 @@ public class ImportObj : MonoBehaviour {
         }
         else{
         	foreach (string s in p){
-        		fileDir += s;
+        		fileDir = p[0];
         	}
         	Debug.Log(fileDir);
         }
@@ -36,22 +41,21 @@ public class ImportObj : MonoBehaviour {
     /**
     *	To-do: mesh-rendering and probably more
     */
-    public void loadModel(string s){
-    	Mesh mesh = FastObjImporter.Instance.ImportFile(s);
-    	Material material = new Material(Shader.Find("Standard")) {
-    		color = new Color(0,0,255)
-    	};
+    public GameObject loadModel(string s){
+    	Debug.Log(s);
+    	Mesh mesh1 = FastObjImporter.Instance.ImportFile(s);
+    	Material mat1 = new Material(Shader.Find("Standard"));
 
     	GameObject gameObj = new GameObject("Model");
-
+    	
+		MeshFilter meshFilter = gameObj.AddComponent<MeshFilter>();
+    	MeshRenderer meshRenderer = gameObj.AddComponent<MeshRenderer>();
+    	meshFilter.mesh = mesh1;
+    	meshRenderer.material = mat1;
+    	
+    	return gameObj;
     }
-
 /*
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		
