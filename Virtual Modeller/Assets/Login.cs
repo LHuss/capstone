ca6446124475beeed03fc67dd8afa523e1eb04a7
Hicham Daou <Hicham_Daou@hotmail.com>
2019-11-04 17:41:32 -0500

    Issue #5 - fix: moved back and quit buttons up and proper indentation in login.cs

--- Virtual Modeller/Assets/Login.cs
+++ Virtual Modeller/Assets/Login.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Login : MonoBehaviour
 {
-
     //Hardcoded Admin Credentials
     private string u = "admin";
     private string p = "admin";
-
     public InputField usernameString;
     public InputField passwordString;
-	public GameObject loginBtn;
-	public GameObject loginMenu;
-	public GameObject mainMenu;
-	public Text welcomeMessage;
+    public GameObject loginBtn;
+    public GameObject loginMenu;
+    public GameObject mainMenu;
+    public Text welcomeMessage;
     public GameObject loginPopup;
 
-
-    public void CheckLogin ()
+    public void CheckLogin()
     {
-        
-        if(u.Equals(usernameString.text)&&p.Equals(passwordString.text))
+        if (u.Equals(usernameString.text) && p.Equals(passwordString.text))
         {
-			Debug.Log("Success, user " + usernameString + " is now logged in.");
-			loginMenu.SetActive (false);
-			mainMenu.SetActive (true);
-			loginBtn.SetActive (false);
-			welcomeMessage.text = "Welcome back " + usernameString + "!";
+            Debug.Log("Success, user " + usernameString + " is now logged in.");
+            loginMenu.SetActive(false);
+            mainMenu.SetActive(true);
+            loginBtn.SetActive(false);
+            welcomeMessage.text = "Welcome back " + usernameString + "!";
         }
         else
         {
-            loginPopup.SetActive (true);
+            loginPopup.SetActive(true);
             Debug.Log(usernameString.text);
             Debug.Log(passwordString.text);
             Debug.Log("Wrong username or password, please try again.");
-		}
-
+        }
     }
-
-
 
 }
