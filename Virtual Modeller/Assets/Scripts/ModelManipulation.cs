0d403c0bd7a5a845e7bcf14a7661483fdc0486e3
Darrin <darrinfong@gmail.com>
2019-11-10 18:07:20 -0500

    Mesh Collider updates on contact, enhanced collision precision

--- Virtual Modeller/Assets/Scripts/ModelManipulation.cs
+++ Virtual Modeller/Assets/Scripts/ModelManipulation.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class ModelManipulation : MonoBehaviour { 
 	
+	private static float COLLISION_PRECISION = 0.05F;	
+
 	void OnCollisionEnter(Collision collision){
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
-		Vector3[] vertices = mesh.vertices;
-		Vector3[] normals = mesh.normals;
+		MeshFilter objectMesh = GetComponent<MeshFilter>();
+		Vector3[] vertices = objectMesh.mesh.vertices;
+		Vector3[] normals = objectMesh.mesh.normals;
 
 		ContactPoint[] contactPoints = collision.contacts;
 		for (int i = 0; i < vertices.Length; i++){
 			Vector3 globalPoint = transform.TransformPoint(vertices[i]);
 			foreach (ContactPoint contact in contactPoints){
-				if (AlmostEqual(contact.point, globalPoint, (float) 0.1)){
+				if (SameGlobalPoint(contact.point, globalPoint)){
 					Vector3 nerfedNormal = new Vector3(
 						contact.normal.x * (float) 0.001,
 						contact.normal.y * (float) 0.001,
@@ -22,15 +24,17 @@ public class ModelManipulation : MonoBehaviour {
 				}
 			}
 		}
-		mesh.vertices = vertices;
-	}	
-	
-	private static bool AlmostEqual(Vector3 v1, Vector3 v2, float precision)
+		objectMesh.mesh.vertices = vertices;
+		objectMesh.mesh.RecalculateNormals();
+		objectMesh.mesh.RecalculateBounds();
+		GetComponent<MeshCollider>().sharedMesh = null;
+		GetComponent<MeshCollider>().sharedMesh = objectMesh.mesh;
+	}
+
+	private static bool SameGlobalPoint(Vector3 v1, Vector3 v2)
 	{
-		bool equal = true;
-		if (Mathf.Abs (v1.x - v2.x) > precision) equal = false;
-		if (Mathf.Abs (v1.y - v2.y) > precision) equal = false;
-		if (Mathf.Abs (v1.z - v2.z) > precision) equal = false;
-		return equal;
+		return !(Mathf.Abs(v1.x - v2.x) > COLLISION_PRECISION ||
+			     Mathf.Abs(v1.y - v2.y) > COLLISION_PRECISION ||
+			     Mathf.Abs(v1.z - v2.z) > COLLISION_PRECISION);
 	}
 }
