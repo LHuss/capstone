00bf78ecb1310b36ff296ed3f7d00dc8af6fe475
hichd <hicham_daou@hotmail.com>
2019-12-29 22:03:35 -0500

    Issue #26 - testing : added test file for object importer

--- Virtual Modeller/Assets/Scripts/ImportObj.cs
+++ Virtual Modeller/Assets/Scripts/ImportObj.cs
@@ -10,18 +10,18 @@ public class ImportObj : MonoBehaviour {
 
 	private string[] path;
 	private string filePath = "";
-    private MeshController meshController;
 
-	public void OpenFile(){
+	public void OpenFile()
+    {
 
 		ExtensionFilter[] extensionList = new [] {
                 new ExtensionFilter("Waveform obj", "obj")
             };
 		path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensionList, false);
-		fileResult(path);
+		FileResult(path);
 
 		if (filePath.Length!=0){
-			importObjFromFile(filePath);
+			ImportObjFromFile(filePath);
 		}
 		else{
 			Debug.Log("Obj file not selected");
@@ -29,46 +29,24 @@ public class ImportObj : MonoBehaviour {
 		
 	}
 
-	public void importObjFromFile(string fp){
+	public void ImportObjFromFile (string fp)
+    {
 		Camera cam = Camera.main;
-
-		//ameObject gameObject = new GameObject("Mesh");
-
-
-        //ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
         ImportOptions importOptions = new ImportOptions();
 		importOptions.buildColliders = true;
 		importOptions.modelScaling = 2f;
-        //ObjectImporter objImporter = gameObject.AddComponent<ObjectImporter>();
         gameObject.AddComponent<ObjectImporter>();
         ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
-
 		objImporter.ImportModelAsync("My Object", filePath, null, importOptions);
         objImporter.ImportedModel += (GameObject importedObject, string path) =>
         {
-            //Do stuff with gameObject
-            importedObject.AddComponent<MeshFilter>();
-            importedObject.AddComponent<MeshCollider>();
-            MeshCollider objMeshCollider = importedObject.GetComponent<MeshCollider>();
-            importedObject.AddComponent<Model>();
-            importedObject.AddComponent<Rigidbody>();
-            Rigidbody rigidbody = importedObject.GetComponent<Rigidbody>();
-            rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
-            rigidbody.isKinematic = true;
-            rigidbody.detectCollisions = true;
-            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-            Debug.Log("Are kinematics enabled?: " + rigidbody.isKinematic);
-            Debug.Log("Are collision detections enabled?: " + rigidbody.detectCollisions);
-            Debug.Log("Current collision detection mode: " + rigidbody.collisionDetectionMode);
-            importedObject.transform.localScale = new Vector3(10, 10, 10);
-            importedObject.transform.Translate(350, 200, 0);
+            //manipulate the asynchronously imported object
+            AttachComponents(importedObject);
+            PositionObject(importedObject);
         };
-        gameObject.transform.position = cam.ScreenToWorldPoint(new Vector3(90f, 160f, -50f));
-
-
 	}
 
-	public void fileResult(string[] p) {
+	public void FileResult (string[] p) {
         if (p.Length == 0) {
         	Debug.Log("file path len = 0");
         }
@@ -76,6 +54,26 @@ public class ImportObj : MonoBehaviour {
         	filePath = p[0];
         	Debug.Log(filePath);
         }
+    }
+
+    public void AttachComponents (GameObject importedObject)
+    {
+        importedObject.AddComponent<MeshFilter>();
+        importedObject.AddComponent<MeshCollider>();
+        MeshCollider objMeshCollider = importedObject.GetComponent<MeshCollider>();
+        importedObject.AddComponent<Model>();
+        importedObject.AddComponent<Rigidbody>();
+        Rigidbody rigidbody = importedObject.GetComponent<Rigidbody>();
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        rigidbody.isKinematic = true;
+        rigidbody.detectCollisions = true;
+        rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+
+    public void PositionObject (GameObject importedObject)
+    {
+        importedObject.transform.localScale = new Vector3(10f, 10f, 10f);
+        importedObject.transform.Translate(350f, 200f, 0f);
     }
 
 }
