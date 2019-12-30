98b52671c458dce9b7e385f7c4ace29b2989d26b
hichd <hicham_daou@hotmail.com>
2019-12-29 20:48:12 -0500

    issue #26 - feat : imported object is now accessible and and components can be added to it

--- Virtual Modeller/Assets/Scripts/ImportObj.cs
+++ Virtual Modeller/Assets/Scripts/ImportObj.cs
@@ -35,22 +35,29 @@ public class ImportObj : MonoBehaviour {
 		gameObject.AddComponent<Rigidbody>();
 		
 		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        //rigidbody.isKinematic = true;
+        rigidbody.detectCollisions = true;
+        rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
 
-		ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
-		ImportOptions importOptions = new ImportOptions();
+        //ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
+        ImportOptions importOptions = new ImportOptions();
 		importOptions.buildColliders = true;
 		importOptions.modelScaling = 2f;
-		objImporter = gameObject.AddComponent<ObjectImporter>();			
-
-		//rigidbody.isKinematic = true;
-		rigidbody.detectCollisions = true;
-		rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        //ObjectImporter objImporter = gameObject.AddComponent<ObjectImporter>();
+        gameObject.AddComponent<ObjectImporter>();
+        ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
 
 		objImporter.ImportModelAsync("My Object", filePath, null, importOptions);
+        objImporter.ImportedModel += (GameObject importedObject, string path) =>
+        {
+            //Do stuff with gameObject
+            importedObject.AddComponent<ModelManipulation>();
+            importedObject.transform.localScale = new Vector3(10, 10, 10);
+            importedObject.transform.Translate(350, 200, 0);
+        };
+        gameObject.transform.position = cam.ScreenToWorldPoint(new Vector3(90f, 160f, -50f));
 
-		gameObject.transform.position = cam.ScreenToWorldPoint(new Vector3(90f, 160f, -50f));
-
-		Debug.Log("Are kinematics enabled?: " + rigidbody.isKinematic);
+        Debug.Log("Are kinematics enabled?: " + rigidbody.isKinematic);
 		Debug.Log("Are collision detections enabled?: " + rigidbody.detectCollisions);
 		Debug.Log("Current collision detection mode: " + rigidbody.collisionDetectionMode);
 	}
@@ -64,5 +71,6 @@ public class ImportObj : MonoBehaviour {
         	Debug.Log(filePath);
         }
     }
+   
 
 }
