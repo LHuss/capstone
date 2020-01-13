646781ba3a03fb3870ad33dbc3c24d0e2ed71ea3
hichd <35614850+hichd@users.noreply.github.com>
2020-01-12 21:13:34 -0500

    Revert "I#26"

---
+++ Virtual Modeller/Assets/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SimpleFileBrowser;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitApplication ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void OpenExplorer()
+    {
+        StartCoroutine(ShowLoadDialogCoroutine());
+    }
+
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+        // Show a load file dialog and wait for a response from user
+        // Load file/folder: file, Initial path: default (Documents), Title: "Load File", submit button text: "Load"
+        yield return FileBrowser.WaitForLoadDialog(false, null, "Load File", "Load");
+
+        // Dialog is closed
+        // Print whether a file is chosen (FileBrowser.Success)
+        // and the path to the selected file (FileBrowser.Result) (null, if FileBrowser.Success is false)
+        Debug.Log(FileBrowser.Success + " " + FileBrowser.Result);
+
+        if (FileBrowser.Success)
+        {
+            // If a file was chosen, read its bytes via FileBrowserHelpers
+            // Contrary to File.ReadAllBytes, this function works on Android 10+, as well
+            byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result);
+        }
+    }
+
+}
--- Virtual Modeller/Assets/Scripts/ImportObj.cs
+++ Virtual Modeller/Assets/Scripts/ImportObj.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using UnityEngine;
 using SFB; // StandaloneFileBrowser
 using AsImpL;
 
@@ -6,41 +10,52 @@ public class ImportObj : MonoBehaviour {
 
 	private string[] path;
 	private string filePath = "";
-    
-	public void OpenFile ()
-    {
+
+	public void OpenFile(){
+
 		ExtensionFilter[] extensionList = new [] {
                 new ExtensionFilter("Waveform obj", "obj")
             };
 		path = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensionList, false);
-		FileResult(path);
+		fileResult(path);
+
 		if (filePath.Length!=0){
-			ImportObjFromFile(filePath);
+			importObjFromFile(filePath);
 		}
 		else{
 			Debug.Log("Obj file not selected");
 		}
+		
 	}
 
-	public void ImportObjFromFile (string fp)
-    {
+	public void importObjFromFile(string fp){
 		Camera cam = Camera.main;
-        ImportOptions importOptions = new ImportOptions();
+
+		GameObject gameObject = new GameObject("Mesh");
+		gameObject.AddComponent<Rigidbody>();
+		
+		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+
+		ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
+		ImportOptions importOptions = new ImportOptions();
 		importOptions.buildColliders = true;
 		importOptions.modelScaling = 2f;
-        gameObject.AddComponent<ObjectImporter>();
-        ObjectImporter objImporter = gameObject.GetComponent<ObjectImporter>();
+		objImporter = gameObject.AddComponent<ObjectImporter>();			
+
+		//rigidbody.isKinematic = true;
+		rigidbody.detectCollisions = true;
+		rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+
 		objImporter.ImportModelAsync("My Object", filePath, null, importOptions);
-        objImporter.ImportedModel += (GameObject importedObject, string path) =>
-        {
-            //manipulate the asynchronously imported object
-            AttachComponents(importedObject);
-            PositionObject(importedObject);
-        };
+
+		gameObject.transform.position = cam.ScreenToWorldPoint(new Vector3(90f, 160f, -50f));
+
+		Debug.Log("Are kinematics enabled?: " + rigidbody.isKinematic);
+		Debug.Log("Are collision detections enabled?: " + rigidbody.detectCollisions);
+		Debug.Log("Current collision detection mode: " + rigidbody.collisionDetectionMode);
 	}
 
-	public void FileResult (string[] p)
-    {
+	public void fileResult(string[] p) {
         if (p.Length == 0) {
         	Debug.Log("file path len = 0");
         }
@@ -48,26 +63,6 @@ public class ImportObj : MonoBehaviour {
         	filePath = p[0];
         	Debug.Log(filePath);
         }
-    }
-
-    public void AttachComponents (GameObject importedObject)
-    {
-        importedObject.AddComponent<MeshFilter>();
-        importedObject.AddComponent<MeshCollider>();
-        MeshCollider objMeshCollider = importedObject.GetComponent<MeshCollider>();
-        importedObject.AddComponent<Model>();
-        importedObject.AddComponent<Rigidbody>();
-        Rigidbody rigidbody = importedObject.GetComponent<Rigidbody>();
-        rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
-        rigidbody.isKinematic = true;
-        rigidbody.detectCollisions = true;
-        rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-    }
-
-    public void PositionObject (GameObject importedObject)
-    {
-        importedObject.transform.localScale = new Vector3(10f, 10f, 10f);
-        importedObject.transform.Translate(350f, 200f, 0f);
     }
 
 }
