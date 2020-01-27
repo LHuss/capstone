14e5bf7e04a679c5eb64336ea67d6cfae3a602b4
hichd <hicham_daou@hotmail.com>
2020-01-26 23:12:02 -0500

    issue #45 - export file ui

---
+++ Virtual Modeller/Assets/Scripts/ExportObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SFB; // StandaloneFileBrowser
+using UnityEngine;
+
+public class ExportObj : MonoBehaviour {
+
+    public void SaveFile()
+    {
+        MeshFilter[] filters = FindObjectsOfType<MeshFilter>();
+        Debug.Log(filters.Length);
+        ExtensionFilter[] extensionList = new[] {
+            new ExtensionFilter("Waveform obj", "obj")
+        };
+        var path = StandaloneFileBrowser.SaveFilePanel("Export OBJ file", "", "ExportedFile", extensionList);
+    }
+}
