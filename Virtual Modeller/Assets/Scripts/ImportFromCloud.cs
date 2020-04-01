3c21f686914abd9a46206e30c7e18b7de58ae582
dizedel <zlliu514@gmail.com>
2020-04-01 17:33:14 -0400

    feat(#7) all functional

--- Virtual Modeller/Assets/Scripts/ImportFromCloud.cs
+++ Virtual Modeller/Assets/Scripts/ImportFromCloud.cs
@@ -7,19 +7,30 @@ using Google.Cloud.Storage.V1;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using System.Windows.Forms;
+using System.Drawing;
 using System;
+using TMPro;
 
 public class ImportFromCloud : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject Panel;
+    public GameObject ScrollView;
+    public TextMeshProUGUI txtMeshObj;
+
+    private StorageClient storage;
+    private string bucketName;
+
+
+    // Use this for initialization
+    void Start () {
         string currLocalDir = Directory.GetCurrentDirectory();
         string default_cred_path = currLocalDir + "\\library\\stately-mote-267221-0b07ba79bf7a.json";
         System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", default_cred_path);
     }
 
     public void OnClick() {
-
+                
         // get list of files in cloud
         List<Google.Apis.Storage.v1.Data.Object> fileList = GetFileList();
 
@@ -27,28 +38,39 @@ public class ImportFromCloud : MonoBehaviour {
         DisplayFiles(fileList);
     }
 
-    // to do
     public void DisplayFiles(List<Google.Apis.Storage.v1.Data.Object> flist) {
+        string allFiles = "";
+        foreach (var file in flist)
+        {
+            int lastIndexOfDot = file.Name.LastIndexOf('.');
+            string name = file.Name.Substring(0, lastIndexOfDot);
+            string ext = file.Name.Substring(lastIndexOfDot + 1);
+            Debug.Log($"found file named {name} with ext {ext}.");
+            if (ext == "obj".ToLower()){
+                allFiles += name + "\n";
+            }
+        }
 
-        // show the files in a dialog
-
-        // import the file selected by the user
-        //DownloadFile(something, something, something);
+        // display panel
+        if (ScrollView != null)
+        {
+            ScrollView.SetActive(true);
+        }
+        txtMeshObj = Panel.GetComponentInChildren<TextMeshProUGUI>();
+        txtMeshObj.text = allFiles;      
 
     }
 
-    // functional
-    public void DownloadFile(string fileToDownload, StorageClient storage, string bucketName) {
+    public void DownloadFile(string fileToDownload, StorageClient sc, string bn) {
         string objectName = fileToDownload;
-        string localPath = Path.GetFileName(objectName); // by default, save the file to the virtual modeller's root directory, will change later
+        string localPath = Directory.GetCurrentDirectory() + "\\" + Path.GetFileName(objectName);
         using (var outputFile = File.OpenWrite(localPath))
         {
-            storage.DownloadObject(bucketName, objectName, outputFile);
+            sc.DownloadObject(bn, objectName, outputFile);
         }
         Debug.Log($"downloaded {objectName} to {localPath}.");
     }
-
-    // functional
+    
     public List<Google.Apis.Storage.v1.Data.Object> GetFileList() {
 
         List<Google.Apis.Storage.v1.Data.Object> filesInCloud = new List<Google.Apis.Storage.v1.Data.Object>();
@@ -56,26 +78,8 @@ public class ImportFromCloud : MonoBehaviour {
         string currLocalDir = Directory.GetCurrentDirectory();
 
         string projectId = "stately-mote-267221";
-        string bucketName = "vmod_bucket";
-        
-        /*
-        
-        string secretId = "vmod_sec";
-        string secretVersion = "latest";        
-
-        // Create the request.
-        var request = new AccessSecretVersionRequest
-        {
-            SecretVersionName = new SecretVersionName(projectId, secretId, secretVersion),
-        };
-
-        SecretManagerServiceClient client = SecretManagerServiceClient.Create();
-
-        var version = client.AccessSecretVersion(request);
-        string payload = version.Payload.Data.ToStringUtf8();
-        Debug.Log($"Payload: {payload}");
-        */          
-        
+        bucketName = "vmod_bucket";
+         
         string credentialsJson = "";
         
         using (StreamReader r = new StreamReader(currLocalDir + "\\library\\stately-mote-267221-0b07ba79bf7a.json"))
@@ -84,7 +88,7 @@ public class ImportFromCloud : MonoBehaviour {
         }
 
         GoogleCredential cred = GoogleCredential.FromJson(credentialsJson);
-        StorageClient storage = StorageClient.Create(cred);
+        storage = StorageClient.Create(cred);
 
         Debug.Log(storage.Service);
 
@@ -93,26 +97,23 @@ public class ImportFromCloud : MonoBehaviour {
         var storageObject = storage.ListObjects(bucketName, "");
         foreach (var stObj in storageObject){
             filesInCloud.Add(stObj);
-            Debug.Log($"found {stObj.Name}");
+            //Debug.Log($"found {stObj.Name}");
         }
         
-
-        // This part is only here temporarily while I test if download works
-        string objectName = "teapot.obj";
-        string localPath = Directory.GetCurrentDirectory() + "\\" + Path.GetFileName(objectName); // by default, save the file to the virtual modeller's root directory, will change later
-        using (var outputFile = File.OpenWrite(localPath))
-        {
-            storage.DownloadObject(bucketName, objectName, outputFile);
-        }
-        Debug.Log($"downloaded {objectName} to {localPath}.");   
-        // =============
-
-
         return filesInCloud;
-    }	
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Update(){
+        if (ScrollView.active == true){
+            if (Input.GetMouseButtonDown(0)){
+                var wordIndex = TMP_TextUtilities.FindIntersectingWord(txtMeshObj, Input.mousePosition, null);
+                string selectedFile;
+                if (wordIndex != -1){
+                    selectedFile = txtMeshObj.textInfo.wordInfo[wordIndex].GetWord() + ".obj";
+                    DownloadFile(selectedFile, storage, bucketName);
+                }
+            }
+        }
+    }
+
 }
