c25a26741e135549dbabd03b85efaaefda7393ec
Darrin <darrinfong@gmail.com>
2020-04-14 17:33:20 -0400

    #65 Created in game button that opens file browser, selected obj file will be used to generate gcode

--- Virtual Modeller/Assets/Scripts/Print/Slic3r_print.sh
+++ Virtual Modeller/Assets/Scripts/Print/Slic3r_print.sh
@@ -1,1 +1,7 @@
-$slic3r -load Virtual_Modeller_config_bundle.ini -scale 150 --support-material --output ../../../obj/print.gcode ../../../obj/MySaveFile.obj > script_output.txt
+gcode="$1";
+obj="$2";
+echo "Object selected: $obj";
+echo "Output path: $gcode";
+$slic3r -load Virtual_Modeller_config_bundle.ini -scale 150 --support-material --output "$gcode" "$obj";
+
+sleep 10;
